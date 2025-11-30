3e6af3bc5ea6bdb0486dfe5b06b75659e0a54491
Lingababu30 <lingababuaithagoni30@gmail.com>
2025-11-30 23:59:11 +0530

    Update webhook message in Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello pipeline");
         System.out.println("checking h/2 ");
-        System.out.println("from webhook");
+        System.out.println("from webhook 2");
     }
 }
